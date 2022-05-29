42ea654d45863317e9f222e2d816ce753de65e4c
Eton Williams <eton.williams@gmail.com>
2022-05-29 17:39:32 +0200

    Create many_escape_sequences.cpp

---
+++ 2 Fundamental Types, Constants and Variables/many_escape_sequences.cpp
@@ -0,0 +1,18 @@
+/*
+many_escape_sequences.cpp
+Prinz 
+Chp 2
+p.26
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "\nThis is\t\v a string\t\v"
+    
+    "\033 with \"many\" escape sequeces!\n";
+
+  return 0;
+}
